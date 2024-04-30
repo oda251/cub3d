38f435af7eee24263d152b7016e1bcb723ff1f42
Yuya Oda <yoda@c1r7s1.42tokyo.jp>
2024-04-30 21:55:18 +0900

    a

--- src/loop/rotate_angle.c
+++ src/loop/rotate_angle.c
@@ -6,8 +6,19 @@
 /*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:02:58 by yoda              #+#    #+#             */
-/*   Updated: 2024/04/30 21:41:21 by yoda             ###   ########.fr       */
+/*   Updated: 2024/04/30 21:54:28 by yoda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "loop.h"
+
+void	rotate_angle(t_data *data, t_direction direction)
+{
+	if (direction == LEFT)
+		data->player.angle = plus_angle(data->player.angle, ROTATE_RATE);
+	else if (direction == RIGHT)
+		data->player.angle = plus_angle(data->player.angle, -ROTATE_RATE);
+	else
+		exit_with_message("rotate_angle", "Invalid direction.");
+	angle_to_vector(data->player.angle, &data->player.direction);
+}
